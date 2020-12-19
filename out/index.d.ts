fbc44a29aadc3de80da2158215a728c3d8c7e5a2
mai.shu <ansiboy@163.com>
2020-12-19 13:26:40 +0800

    移除 out 文件夹

--- out/index.d.ts
+++
@@ -1,8 +0,0 @@
-import "reflect-metadata";
-export { controller, action, createParameterDecorator, routeData, request, response, serverContext } from "./attributes";
-export { ActionResult, ServerContext } from "./types";
-export { Controller } from "./controller";
-export { createVirtualDirecotry } from "./virtual-directory";
-export { ContentResult, RedirectResult, ProxyResut } from "./action-results";
-export { LogLevel, getLogger } from "./logger";
-export { MVCRequestProcessorConfig, MVCRequestProcessor } from "./mvc";
--- out/logger.js
+++
@@ -1,4 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var maishu_node_web_server_1 = require("maishu-node-web-server");
-exports.getLogger = maishu_node_web_server_1.getLogger;
